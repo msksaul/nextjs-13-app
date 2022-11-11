04e2e322850dd9c28e57710a2b48f3e0f33204db
mscalel <msksaul@gmail.com>
2022-11-11 17:33:22 -0600

    React Server Components

--- app/posts/page.jsx
+++ app/posts/page.jsx
@@ -1,5 +1,11 @@
-export default function PostsPage() {
+import { ListOfPosts } from './ListOfPosts';
+
+export default async function PostsPage() {
+
+
   return (
-    <h1>posts</h1>
+    <section>
+      <ListOfPosts />
+    </section>
   )
 }
