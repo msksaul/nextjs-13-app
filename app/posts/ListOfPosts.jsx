33268949462d36022dc2312e75ead6027359624f
mscalel <msksaul@gmail.com>
2022-11-12 15:00:05 -0600

    Layout State

--- app/posts/ListOfPosts.jsx
+++ app/posts/ListOfPosts.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { LikeButton } from './LikeButton'
 
 const fetchPosts = () => {
@@ -14,9 +15,11 @@ export async function ListOfPosts() {
 
   return posts.slice(0,5).map(post => (
     <article key={post.id}>
-      <h2 style={{ color: '#09f' }}>{post.title}</h2>
-      <p>{post.body}</p>
-      <LikeButton id={post.id}/>
+      <Link href='/posts/[id]' as={`/posts/${post.id}`}>
+        <h2 style={{ color: '#09f' }}>{post.title}</h2>
+        <p>{post.body}</p>
+        <LikeButton id={post.id}/>
+      </Link>
     </article>
   ))
 }
